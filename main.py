fefbeea752665ddf4383b1516fe88a0150599881
Christian Motta <chrstn.motta@gmail.com>
2026-05-19 17:41:26 +0200

    WIP: PDF upload endpoint, extraction not working yet

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from typing import Annotated
+from pypdf import PdfReader
+import json
 
 app = FastAPI()
 
@@ -6,7 +9,30 @@ app = FastAPI()
 def hello(name: str):
     return {"message": f"Hello {name}"}
 
-@app.get("/hello/add/{a}/{b}")
-def add(a: int, b:int):
-    sum = a + b
-    return {"result sum": sum}
+#Doel Een pdf kunnen opsturen naar API (software) en tekst extracteren 
+
+# Pseudocode
+# 1. Ontvang PDF op POST (data receiver for API)
+# 2. UploadFile om de files daadwerkelijk te uploaden naar api
+# 3. Specifieer type data (tekst)
+# 4. Open pdf 
+# 5. Parse pdf 
+# 6. Loop door de paginas
+# 7. Extract de tekst 
+# 8. Convert extracted to JSON
+
+def extract_pdf(filename):
+    reader = PdfReader(str(filename))
+    pages = reader.pages[:]
+    for page in pages:
+        pdf_text += page.extract_text()
+    return pdf_text
+   
+@app.post("/files/")
+async def create_file(file: Annotated[bytes, File()]):
+    return {"file_size": len(file)}
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    extract_pdf(file)
+    return {"filename": json.dumps(file.filename)}
